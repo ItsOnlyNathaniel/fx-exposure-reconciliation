61ca8831ad1e2cb32dddb2b9365978d681a65a66
ItsOnlyNathaniel <2125418@swansea.ac.uk>
2026-03-17 12:08:58 +0000

    Refactored CSV and JSON data ingestion into a new file_reader module; removed deprecated csv_reader and json_reader files. Update tests to utilize the new file_reader functions.

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,15 +1,14 @@
 import pytest
 import reconciliation.engine as engine
 import synthetic.generate_dataset as generate_dataset
-import ingestion.csv_reader as csv_reader
-import ingestion.json_reader as json_reader
+import ingestion.file_reader as file_reader
 
 
 def test_data_generation_and_ingestion():
     generate_dataset.main()
-    feed_df = csv_reader.parse_csv(csv_reader.read_csv())
-    ledger_df = json_reader.parse_json(json_reader.read_json())
+    feed_df = file_reader.parse(file_reader.read_csv())
+    ledger_df = file_reader.parse(file_reader.read_json())
     breaks = engine.reconcile_trades(feed_df, ledger_df)
-    print(breaks)
+    
     assert not breaks.empty
     assert "trade_id" in breaks.columns
